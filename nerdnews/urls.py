862227ef97fa15b06f8fee7ed1200071e09d638e
Sayani Bhattacharjee <qwerty.sayani@gmail.com>
2018-09-27 13:16:49 +0530

    nerdnews: Add the templates with the modified UI

--- nerdnews/urls.py
+++ nerdnews/urls.py
@@ -15,9 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include, re_path
+from django.conf.urls.static import static
+from django.conf import settings
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     re_path(r'', include('dais.urls'))
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
